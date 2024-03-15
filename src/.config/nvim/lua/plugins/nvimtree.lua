014437614f6014f8cc8a8af4d6a2f25df86a960b
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2024-03-15 01:48:26 +0000

    Don't resolve symlinks, wider in nvim-tree

--- src/.config/nvim/lua/plugins/nvimtree.lua
+++ src/.config/nvim/lua/plugins/nvimtree.lua
@@ -5,7 +5,14 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     config = function()
-      require("nvim-tree").setup()
+      require("nvim-tree").setup {
+        view = {
+          width = 45
+        },
+        renderer = {
+          symlink_destination = false
+        }
+      }
     end
   },
 }
