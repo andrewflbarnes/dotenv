f44f4f50d30426811bca8101cc2c5fddd38936a7
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2024-06-04 00:32:22 +0100

    Fix volar 2 to ensure js/ts works in vue files

--- src/.config/nvim/lua/plugins/lsp.lua
+++ src/.config/nvim/lua/plugins/lsp.lua
@@ -53,6 +53,19 @@ return {
             single_file_support = false,
           }
         end,
+        ["volar"] = function()
+          require("lspconfig").volar.setup({
+            filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+            init_options = {
+              vue = {
+                hybridMode = false,
+              },
+              typescript = {
+                tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+              },
+            },
+          })
+        end,
       }
     end,
   },
